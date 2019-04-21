3cbcf56c29a7b8268d8c8964c9bd2ddce3803d43
natalie-nmt <natalie.thomas.nmt@gmail.com>
2019-04-20 21:07:00 -0400

    initial commit

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -26,7 +26,7 @@ module.exports = {
           const msg = {
             to: 'natalie.thomas.nmt@gmail.com',
             from: 'natalie.thomas.nmt@gmail.com',
-            subject: 'Sending with SendGrid is Fun',
+            subject: 'Welcome to Blocipedia!',
             text: 'and easy to do anywhere, even with Node.js',
             html: '<strong>and easy to do anywhere, even with Node.js</strong>',
           };
