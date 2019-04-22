539edbb91ea4e91260359cdfde4c8e5ea7a979f2
natalie-nmt <natalie.thomas.nmt@gmail.com>
2019-04-21 21:13:31 -0400

    Edited cookie settings

--- src/config/main-config.js
+++ src/config/main-config.js
@@ -9,26 +9,26 @@ const flash = require("express-flash");
 const logger = require('morgan');
 
 module.exports = {
-  init(app,express){
+  init(app, express) {
     app.set("views", viewsFolder);
     app.set("view engine", "ejs");
     app.use(bodyParser.urlencoded({ extended: true }));
     app.use(express.static(path.join(__dirname, "..", "assets")));
     app.use(expressValidator());
-        app.use(session({
-      secret: process.env.cookieSecret,
+    app.use(session({
+      secret: "test secret",
       resave: false,
       saveUninitialized: false,
       cookie: { maxAge: 1.21e+9 }
     }));
-    app.use(flash()); 
+    app.use(flash());
     passportConfig.init(app);
- 
+
     app.use((req, res, next) => {
       res.locals.currentUser = req.user;
       next();
     })
 
-    app.use(logger('dev'));  
+    app.use(logger('dev'));
   }
 };
