edc0dc71f771d30decdf67ca0dbdff16e638b803
natalie-nmt <natalie.thomas.nmt@gmail.com>
2019-04-22 22:32:22 -0400

    Upgraded Sequelize syntax to resolve findById error

--- src/config/passport-config.js
+++ src/config/passport-config.js
@@ -30,7 +30,8 @@ module.exports = {
     });
 
     passport.deserializeUser((id, callback) => {
-      User.findById(id)
+      console.log("Debugging: The user is", User);
+      User.findByPk(id)
         .then((user) => {
           callback(null, user);
         })
